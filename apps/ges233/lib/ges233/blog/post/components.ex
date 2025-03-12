b3de94b0353a4ed3b22ac574f28cae749f22348a
RoastedChestnut <chestnut.roasted@outlook.com>
2025-03-12 13:09:13 +0800

    添加了其他信息

--- apps/ges233/lib/ges233/blog/post/components.ex
+++ apps/ges233/lib/ges233/blog/post/components.ex
@@ -80,6 +80,18 @@ defmodule GES233.Blog.Post.Components do
     """
     <article>
       <a href="#{Post.post_id_to_route(post)}"><h3>#{post.title}</h3></a>
+      <p>
+        <span>
+          <svg width="14px" height="14px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>
+          <small>#{NaiveDateTime.to_date(post.create_at) |> Date.to_string()}</small>
+        </span>
+        <span>#{appearance_status(post.progress)}</span>
+      </p>
+      <div>
+        #{post.body}
+      </div>
     </article>
     """
   end
