0e608f55c9521210a0c0af259346b69afc789cd0
RoastedChestnut <chestnut.roasted@outlook.com>
2025-10-13 11:44:49 +0800

    做做样子

--- apps/pandox/lib/lilypond.ex
+++ apps/pandox/lib/lilypond.ex
@@ -12,9 +12,14 @@ defmodule Lilypond do
     end
   end
 
+  def execute(_args) do
+    System.shell("#{get_ly()}")
+    |> handle_result()
+  end
+
   defp args(:version, _), do: "-v"
   # defp args(:snippet, {source, target}), do: ~w()
 
-  # defp handle_result({res, 0}), do: {:ok, res}
-  # defp handle_result({err, code}), do: {:error, {code, err}}
+  defp handle_result({res, 0}), do: {:ok, res}
+  defp handle_result({err, code}), do: {:error, {code, err}}
 end
