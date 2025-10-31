b9a632a01b2b510fa5baefd07e166224bae9e432
RoastedChestnut <chestnut.roasted@outlook.com>
2025-10-31 09:38:14 +0800

    更新了 mix g.gen 任务

--- apps/ges233/lib/mix/tasks/gen.ex
+++ apps/ges233/lib/mix/tasks/gen.ex
@@ -1,9 +1,20 @@
 defmodule Mix.Tasks.G.Gen do
   use Mix.Task
 
-  def run(_) do
-    Mix.Task.run("app.start", [])
+  require Logger
 
-    GES233.exe()
+  def run(_) do
+    Application.get_env(:ges233, :saved_path)
+    |> File.exists?()
+    |> unless(do: File.mkdir!(Application.get_env(:ges233, :saved_path)))
+
+    opts = [strategy: :one_for_one, name: GES233.Supervisor]
+
+    {:ok, _supervisor_id} = Supervisor.start_link([{GES233.Blog.ContentRepo, []}], opts)
+
+    # 执行最开始的一次构建任务
+    Logger.info("Performing initial site build...")
+    _initial_context = GES233.Blog.Builder.build_from_root()
+    Logger.info("Initial build complete.")
   end
 end
