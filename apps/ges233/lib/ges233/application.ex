33a71791970704e6615294464607b5e994bafe61
RoastedChestnut <chestnut.roasted@outlook.com>
2025-11-02 11:28:59 +0800

    feat(ges233): add supervisor timeout and server configurationall interfaces (`ip: :any`) and port 6969

--- apps/ges233/lib/ges233/application.ex
+++ apps/ges233/lib/ges233/application.ex
@@ -16,20 +16,20 @@ defmodule GES233.Application do
       {GES233.Blog.Broadcaster, []}
     ]
 
-    opts = [strategy: :one_for_one, name: GES233.Supervisor]
+    opts = [strategy: :one_for_one, name: GES233.Supervisor, max_seconds: 30]
 
     {:ok, supervisor_id} = Supervisor.start_link(children, opts)
 
     # 执行最开始的一次构建任务
     Logger.info("Performing initial site build...")
-    initial_context = GES233.Blog.Builder.build_from_root()
-    Logger.info("Initial build complete.")
+    {elapse, initial_context} = :timer.tc(&GES233.Blog.Builder.build_from_root/0, :millisecond)
+    Logger.info("Initial build complete, elapsed #{inspect(elapse)} ms.")
 
     watcher_spec = {GES233.Blog.Watcher, initial_context}
     {:ok, _watcher_pid} = Supervisor.start_child(GES233.Supervisor, watcher_spec)
 
     if Application.get_env(:ges233, :saved_path) |> File.ls!() |> length() > 0 do
-      server_spec = {Bandit, scheme: :http, plug: GES233.Blog.SimpleServer}
+      server_spec = {Bandit, scheme: :http, plug: GES233.Blog.SimpleServer, ip: :any, port: 6969}
       {:ok, _server_pid} = Supervisor.start_child(GES233.Supervisor, server_spec)
       Logger.info("Starting web server.")
     end
